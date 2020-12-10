19f6c741f8592d9a08e10e5c856cffef418cdd15
François Baldassari <francois@memfault.io>
2020-12-10 00:20:59 -0800

    Day 10: some cleanup

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -10,40 +10,38 @@ use std::collections::{HashMap};
 fn main() {
     let f = File::open("input.txt").expect("Unable to open input");
     let buf = BufReader::new(f);
-    let adapters = buf.lines().map(|l| l.unwrap().parse().unwrap()).sorted();
+    let mut adapters: Vec<usize> = buf.lines().map(|l| l.unwrap().parse().unwrap()).sorted().collect();
+    // add last node
+    adapters.push(adapters.last().unwrap() + 3);
 
     // part 1
     let mut last = 0;
     let mut sums = [0, 0, 0];
-    for adapter in adapters.clone() {
-        sums[(adapter - last - 1) as usize] += 1;
-        last = adapter;
+    for adapter in &adapters {
+        sums[adapter - last - 1] += 1;
+        last = *adapter;
     }
-    // add device (always 3 away)
-    sums[2] += 1;
     println!("part 1 {}", sums[0] * sums[2]);
 
     // part 2
     let mut graph = DefaultGraph::new();
     let mut trailing: Vec<usize> = [0].to_vec();
     graph.add_node(0).unwrap();
-    for adapter in adapters.clone() {
-        graph.add_node(adapter).unwrap();
+    for adapter in &adapters {
+        graph.add_node(*adapter).unwrap();
         for node in &trailing {
             if adapter - node <= 3 {
-                graph.add_edge(adapter, *node).unwrap();
+                graph.add_edge(*adapter, *node).unwrap();
             }
         }
-        trailing.push(adapter);
+        trailing.push(*adapter);
         if trailing.len() > 3 {
             trailing.remove(0);
         }
     }
-    let last = adapters.last().unwrap();
-    graph.add_node(last + 3).unwrap();
-    graph.add_edge(last, last + 3).unwrap();
 
     fn get_children(graph: &DefaultGraph, node: &usize) -> Vec<usize> {
+        // graph library gives me neighbors, which includes parents
         let children: Vec<usize> = graph.neighbors(*node).unwrap().iter().filter(|n| *n > node).map(|n| *n).collect();
         return children
     }
@@ -63,5 +61,5 @@ fn main() {
     }
 
     let mut paths_cache: HashMap<usize, u64> = HashMap::new();
-    println!("Part 2: {}", count_paths(&graph, &mut paths_cache, 0, last + 3));
+    println!("Part 2: {}", count_paths(&graph, &mut paths_cache, 0, last));
 }
