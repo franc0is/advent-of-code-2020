d6259d1d3601ddf01c47eb842fa839800ceb5a67
François Baldassari <francois@memfault.io>
2020-12-12 19:58:19 -0800

    Day 11: Rewrite WIP - it compiles but its wrong

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,259 +1,318 @@
 use std::{
+    ops::Add,
     fs::File,
     io::{prelude::*, BufReader}
 };
 
+#[derive(Copy, Clone)]
+struct Point {
+    x: i32,
+    y: i32
+}
 
+impl Add for Point {
+    type Output = Self;
 
-#[derive(PartialEq, Debug, Clone)]
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
 enum SeatState {
     FLOOR,
     OCCUPIED,
     EMPTY,
 }
 
-type SeatMap = Vec<Vec<SeatState>>;
-
-fn char_to_seat_state(c: &char) -> SeatState {
-    match  c {
-        'L' => SeatState::EMPTY,
-        '.' => SeatState::FLOOR,
-        '#' => SeatState::OCCUPIED,
-        _ => panic!("Invalid input {}", c)
+impl SeatState {
+    fn from_char(c: &char) -> SeatState {
+        match  c {
+            'L' => SeatState::EMPTY,
+            '.' => SeatState::FLOOR,
+            '#' => SeatState::OCCUPIED,
+            _ => panic!("Invalid seat input {}", c)
+        }
     }
 }
 
-fn count_state(seatmap: &SeatMap, state: SeatState) -> usize {
-    return seatmap.iter().flatten().fold(0, |acc, s| {
-        if *s == state {
-            acc + 1
-        } else {
-            acc
+impl std::fmt::Display for SeatState {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            SeatState::EMPTY => write!(f, "L"),
+            SeatState::FLOOR => write!(f, "."),
+            SeatState::OCCUPIED => write!(f, "#")
         }
-    });
+    }
 }
 
-//fn seat_map_to_matrix(seatmap: &SeatMap) -> Vec<Vec<usize>> {
-//    return seatmap.clone().iter().map(|row| row.iter().map(|seat| match seat {
-//        SeatState::EMPTY | SeatState::FLOOR => 0,
-//        SeatState::OCCUPIED => 1
-//    }).collect()).collect();
+struct SeatMap {
+    width: i32,
+    height: i32,
+    seats: Vec<SeatState>,
+}
+
+impl SeatMap {
+    const ADJACENT_MAP: [Point;8] = [
+        Point { x: -1, y: -1 },
+        Point { x:  0, y: -1 },
+        Point { x:  1, y: -1 },
+        Point { x: -1, y:  0 },
+        Point { x:  1, y:  0 },
+        Point { x: -1, y:  1 },
+        Point { x:  0, y:  1 },
+        Point { x:  1, y:  1 },
+    ];
+
+    fn get(&self, p: Point) -> Option<SeatState> {
+        if p.x < 0 || p.x >= self.width ||
+           p.y < 0 || p.y >= self.height {
+            // out of bounds
+            return None;
+        }
+
+        let i = (p.y * self.width + p.x) as usize;
+        Some(self.seats[i])
+    }
+
+    fn get_adjacent(&self, p: Point) -> Vec<SeatState> {
+        return Self::ADJACENT_MAP.iter()
+                                 .map(|adjacent| self.get(*adjacent + p))
+                                 .filter(|opt| opt.is_some())
+                                 .map(|opt| opt.unwrap())
+                                 .collect();
+    }
+
+    fn count_adjacent(&self, p: Point) -> usize {
+        self.get_adjacent(p).iter().fold(0, |acc, adjacent| {
+            match adjacent {
+                SeatState::EMPTY | SeatState::FLOOR => {
+                    acc
+                },
+                SeatState::OCCUPIED => {
+                    acc + 1
+                }
+            }
+        })
+    }
+
+    fn count_occupied(&self) -> usize {
+        self.seats.iter().fold(0, |acc, s| {
+            if *s == SeatState::OCCUPIED {
+                acc + 1
+            } else {
+                acc
+            }
+        })
+    }
+}
+
+
+impl std::fmt::Display for SeatMap {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for (i, seat) in self.seats.iter().enumerate() {
+            if i != 0 && i % (self.width as usize) == 0 {
+                write!(f, "\n");
+            }
+            write!(f, "{}", seat);
+        }
+        Ok(())
+    }
+}
+
+
+//fn count_adjacent_part2(seatmap: &SeatMap, i: usize, j: usize) -> usize {
+//    let h = seatmap.len();
+//    let w = seatmap[0].len();
+//    let mut count = 0;
+//
+//    // look right
+//    for k in j..w {
+//        if (i == 2 && j == 0) {
+//            //println!("right {} {}: {:?}", i, k, seatmap[i][k]);
+//        }
+//        if k != j && seatmap[i][k] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != j && seatmap[i][k] == SeatState::EMPTY {
+//            break;
+//        }
+//    }
+//    // look left
+//    for k in (0..j+1).rev() {
+//        if (i == 2 && j == 0) {
+//            //println!("left {} {}: {:?}", i, k, seatmap[i][k]);
+//        }
+//        if k != j && seatmap[i][k] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != j && seatmap[i][k] == SeatState::EMPTY {
+//            break;
+//        }
+//
+//    }
+//    // look down
+//    for k in i..h {
+//        if (i == 2 && j == 0) {
+//            //println!("down {} {}: {:?}", k, j, seatmap[k][j]);
+//        }
+//        if k != i && seatmap[k][j] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != i && seatmap[k][j] == SeatState::EMPTY {
+//            break;
+//        }
+//    }
+//    // look up
+//    for k in (0..i+1).rev() {
+//        if (i == 2 && j == 0) {
+//            //println!("up {} {}: {:?}", k, j, seatmap[k][j]);
+//        }
+//        if k != i && seatmap[k][j] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != i && seatmap[k][j] == SeatState::EMPTY {
+//            break;
+//        }
+//    }
+//    // look top-right
+//    for (k, l) in (0..i+1).rev().zip(j..w) {
+//        if (i == 2 && j == 0) {
+//            //println!("tr {} {}: {:?}", k, l, seatmap[k][l]);
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
+//            break;
+//        }
+//    }
+//    // look bottom right
+//    for (k, l) in (i..h).zip(j..w) {
+//        if (i == 2 && j == 0) {
+//            //println!("br {} {}: {:?}", k, l, seatmap[k][l]);
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
+//            break;
+//        }
+//    }
+//    // look bottom  left
+//    for (k, l) in (i..h).zip((0..j+1).rev()) {
+//        if (i == 2 && j == 0) {
+//            //println!("bl {} {}: {:?}", k, l, seatmap[k][l]);
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
+//            break;
+//        }
+//    }
+//    // look top left
+//    for (k, l) in (0..i+1).rev().zip((0..j+1).rev()) {
+//        if (i == 2 && j == 0) {
+//            //println!("tl {} {}: {:?}", k, l, seatmap[k][l]);
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
+//            count += 1;
+//            break;
+//        }
+//        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
+//            break;
+//        }
+//    }
+//
+//    if (i == 2 && j == 0) {
+//        //println!("{} {}: {}", i, j, count);
+//    }
+//    return count;
 //}
 
-fn count_adjacent_part1(seatmap: &SeatMap, i: usize, j: usize) -> usize {
-    let h = seatmap.len() - 1;
-    let w = seatmap[0].len() - 1;
-    let mut adjacent: Vec<&SeatState> = Vec::new();
-
-    if i > 0 && j > 0 {
-        adjacent.push(&seatmap[i - 1][j - 1]);
-    }
-    if i < h && j < w {
-        adjacent.push(&seatmap[i + 1][j + 1]);
-    }
-    if i > 0 && j < w {
-        adjacent.push(&seatmap[i - 1][j + 1]);
-    }
-    if i < h && j > 0 {
-        adjacent.push(&seatmap[i + 1][j - 1]);
-    }
-    if i > 0 {
-        adjacent.push(&seatmap[i - 1][j]);
-    }
-    if j > 0 {
-        adjacent.push(&seatmap[i][j - 1]);
-    }
-    if i < h {
-        adjacent.push(&seatmap[i + 1][j]);
-    }
-    if j < w {
-        adjacent.push(&seatmap[i][j + 1]);
-    }
-
-    return adjacent.iter().fold(0, |acc, seat| match seat {
-        SeatState::EMPTY | SeatState::FLOOR => {
-            acc
-        },
-        SeatState::OCCUPIED => {
-            acc + 1
-        }
-    });
-}
-
-fn count_adjacent_part2(seatmap: &SeatMap, i: usize, j: usize) -> usize {
-    let h = seatmap.len();
-    let w = seatmap[0].len();
-    let mut count = 0;
-
-    // look right
-    for k in j..w {
-        if (i == 2 && j == 0) {
-            //println!("right {} {}: {:?}", i, k, seatmap[i][k]);
-        }
-        if k != j && seatmap[i][k] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != j && seatmap[i][k] == SeatState::EMPTY {
-            break;
-        }
-    }
-    // look left
-    for k in (0..j+1).rev() {
-        if (i == 2 && j == 0) {
-            //println!("left {} {}: {:?}", i, k, seatmap[i][k]);
-        }
-        if k != j && seatmap[i][k] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != j && seatmap[i][k] == SeatState::EMPTY {
-            break;
-        }
-
-    }
-    // look down
-    for k in i..h {
-        if (i == 2 && j == 0) {
-            //println!("down {} {}: {:?}", k, j, seatmap[k][j]);
-        }
-        if k != i && seatmap[k][j] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != i && seatmap[k][j] == SeatState::EMPTY {
-            break;
-        }
-    }
-    // look up
-    for k in (0..i+1).rev() {
-        if (i == 2 && j == 0) {
-            //println!("up {} {}: {:?}", k, j, seatmap[k][j]);
-        }
-        if k != i && seatmap[k][j] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != i && seatmap[k][j] == SeatState::EMPTY {
-            break;
-        }
-    }
-    // look top-right
-    for (k, l) in (0..i+1).rev().zip(j..w) {
-        if (i == 2 && j == 0) {
-            //println!("tr {} {}: {:?}", k, l, seatmap[k][l]);
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
-            break;
-        }
-    }
-    // look bottom right
-    for (k, l) in (i..h).zip(j..w) {
-        if (i == 2 && j == 0) {
-            //println!("br {} {}: {:?}", k, l, seatmap[k][l]);
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
-            break;
-        }
-    }
-    // look bottom  left
-    for (k, l) in (i..h).zip((0..j+1).rev()) {
-        if (i == 2 && j == 0) {
-            //println!("bl {} {}: {:?}", k, l, seatmap[k][l]);
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
-            break;
-        }
-    }
-    // look top left
-    for (k, l) in (0..i+1).rev().zip((0..j+1).rev()) {
-        if (i == 2 && j == 0) {
-            //println!("tl {} {}: {:?}", k, l, seatmap[k][l]);
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::OCCUPIED {
-            count += 1;
-            break;
-        }
-        if k != i && l != j && seatmap[k][l] == SeatState::EMPTY {
-            break;
-        }
-    }
-
-    if (i == 2 && j == 0) {
-        //println!("{} {}: {}", i, j, count);
-    }
-    return count;
-}
 
 
-
-fn step(seatmap: &SeatMap) -> (usize, SeatMap) {
-    let h = seatmap.len();
-    let w = seatmap[0].len();
+fn step(seatmap: &SeatMap) -> Option<SeatMap> {
     let mut changes = 0;
-    let mut nm: SeatMap = vec![vec![SeatState::FLOOR; w]; h];
-    for i in 0..h {
-        for j in 0..w {
-            let seat = &seatmap[i][j];
-            if *seat == SeatState::FLOOR {
-                continue;
-            } else {
-                let c = count_adjacent_part2(seatmap, i, j);
-                //println!("count {}", c);
-                if *seat == SeatState::EMPTY && c == 0 {
-                    nm[i][j] = SeatState::OCCUPIED;
-                    changes += 1
-                }  else if *seat == SeatState::OCCUPIED && c >= 5 {
-                    nm[i][j] = SeatState::EMPTY;
-                    changes += 1
+    let next = seatmap.seats.iter().enumerate().map(|(ii, seat)| {
+        let i = ii as i32;
+        let p = Point {x: i % seatmap.width, y: i / seatmap.height };
+        match seat {
+            SeatState::FLOOR => SeatState::FLOOR,
+            SeatState::EMPTY => {
+                if seatmap.count_adjacent(p) == 0 {
+                    changes += 1;
+                    SeatState::OCCUPIED
                 } else {
-                    nm[i][j] = seatmap[i][j].clone();
+                    SeatState::EMPTY
+                }
+            },
+            SeatState::OCCUPIED=> {
+                if seatmap.count_adjacent(p) >= 4 {
+                    changes += 1;
+                    SeatState::EMPTY
+                } else {
+                    SeatState::OCCUPIED
                 }
             }
         }
-    }
+    }).collect();
 
-    return (changes, nm);
+    println!("Step changed {}", changes);
+
+    if changes == 0 {
+        None
+    } else {
+        Some(SeatMap { 
+                height: seatmap.height,
+                width: seatmap.width,
+                seats: next
+            })
+    }
 }
 
 fn main() {
     let f = File::open("input.txt").expect("Unable to open input");
     let buf = BufReader::new(f);
-    let mut seatmap: SeatMap = buf.lines()
-                                  .map(|l| l.unwrap().chars().map(|c| char_to_seat_state(&c)).collect())
-                                  .collect();
+    let chars: Vec<Vec<char>> = buf.lines()
+                                   .map(|l| l.unwrap().chars().collect::<Vec<char>>())
+                                   .collect();
 
-    println!("Start: There are {} seats",  count_state(&seatmap, SeatState::EMPTY));
-    println!("Start: There are {} occupied",  count_state(&seatmap, SeatState::OCCUPIED));
+    let width = chars[0].len() as i32;
+    let height = chars.len() as i32;
+    let seats:Vec<SeatState> = chars.iter()
+                                    .flatten()
+                                    .map(|c| SeatState::from_char(&c))
+                                    .collect();
 
-    //let mut n = 0;
-    //println!("{:?}", seatmap);
-    //let (n, s) = step(&seatmap);
-    //seatmap = s;
-    //println!(">>>>>");
-    //println!("{:?}", seatmap);
-    //let (n, s) = step(&seatmap);
-    //seatmap = s;
+    let mut seatmap = SeatMap { width: width, height: height, seats: seats };
+
+    println!("Start: There are {} occupied",  seatmap.count_occupied());
+
+    let  mut i = 0;
+
     loop {
-        let (n, s) = step(&seatmap);
-        seatmap = s;
-        //println!("{:?}\n", seatmap);
-        if (n == 0) {
+        //println!("{}\n", seatmap);
+        let next = step(&seatmap);
+        if next.is_none() {
             break;
+        } else {
+            seatmap = next.unwrap();
         }
+        //i += 1;
+        //if (i == 10) { break; }
     }
 
-    println!("Part 1: There are {} occupied seats",  count_state(&seatmap, SeatState::OCCUPIED));
+    println!("Part 1: There are {} occupied seats",  seatmap.count_occupied());
 }
